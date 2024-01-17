42fc6cd4c9556cfc4aa52c196f25ba43a96cfdf4
AnaElisaMueller <emailanaelisa@gmail.com>
2024-01-17 18:10:33 -0300

    add parte 2

--- index.php
+++ index.php
@@ -7,17 +7,28 @@ if ( !isset($_SESSION['tasks'])) {
 }
 
 if ( isset ($_GET['task_name'])){
+    if ($_GET['task_name'] != ""){
     array_push($_SESSION['tasks'], $_GET['task_name']);
     unset($_GET['task_name']);
+    } else{
+    $_SESSION['message'] = "O campo nome da tarefa não pode ser vazio!";
+    }
 }
 
 if ( isset ($_GET['clear'])){
     unset($_SESSION['tasks']);
+    unset($_GET['clear']);
+}
 
+if ( isset ($_GET['key'])){
+    array_splice($_SESSION['tasks'], $_GET['key'], 1 );
+    unset($_GET['key']); 
 }
 
 
-//var_dump($_SESSION['tasks']);
+
+
+//var_dump($_SESSION['tasks']); 
 
 
 ?>
@@ -45,6 +56,15 @@ if ( isset ($_GET['clear'])){
             <input type="text" name="task_name" placeholder="Nome da Tarefa">
             <button type="submit">Cadastrar</button>
         </form>
+        <?php
+        if (isset ($_SESSION['message'])){
+            echo "<p style='color: #ca3939';> .$_SESSION['message'] . </p>";
+            unset ($_SESSION['message']);
+        }
+
+
+        ?>
+
     </div>
 
     <div class="separator"></div>
@@ -54,7 +74,19 @@ if ( isset ($_GET['clear'])){
 echo "<ul>";
 
         foreach($_SESSION['tasks'] as $key => $task){
-            echo "<li>$task</li>";
+            echo "<li>
+                <span>$task</span>
+                <button type='button' class='btn-clear' onclick='deletar$key()'>Remover</button>
+                <script>
+                function deletar$key(){
+                    if ( confirm('Confirmar remoção?')){
+                        window.location = 'https://localhost:8100/?key=$key';
+                    }
+                    return false;
+                }
+                </script>
+
+            </li>";
         }
 
 echo "<ul>";
