4de91ee3de84fa1ed08c888dbbe168dc8e6445fb
AnaM <emailanaelisa@gmail.com>
2024-02-10 14:46:49 -0300

    Update details.php

--- details.php
+++ details.php
@@ -1,11 +1,15 @@
 <?php
 
+require __DIR__ . '/connect.php';
+
 session_start();
 
-$data = $_SESSION['tasks'][$_GET['key']];
+$stmt = $conn->prepare("SELECT * FROM tasks WHERE id = :id");
+$stmt->bindParam(':id', $_GET['key']);
+$stmt->execute();
+$data = $stmt->fetchAll();
+
 ?>
-
-
 
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -15,33 +19,35 @@ $data = $_SESSION['tasks'][$_GET['key']];
     <link rel="stylesheet" href="style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,400;0,700;1,400;1,500&display=swap" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;500;700&display=swap" rel="stylesheet">
     <title>Gerenciador de Tarefas</title>
 </head>
 <body>
 
-    <div class="details-container">
-        <div class="header">
-            <h1><?php echo $data['task_name']; ?></h1>
+<div class="details-container">
+    <div class="header">
+        <h1><?php echo $data[0]['task_name']; ?></h1>
+    </div>
+    <div class="row">
+        <div class="details">
+            <dl>
+                <dt>Descrição da tarefa:</dt>
+                <dd><?php echo $data[0]['task_description']  ?></dd>
+                <dt>Data da tarefa:</dt>
+                <dd><?php echo $data[0]['task_date']  ?></dd>
+            </dl>
         </div>
-        <div class="row">
-            <div class="details">
-                <dl>
-                    <dt>Descrição da tarefa:</dt>
-                    <dd><?php echo $data['task_description'] ?></dd>
-                    <dt>Data da tarefa:</dt>
-                    <dd><?php echo $data['task_date'] ?></dd>
-                </dl>
-            </div>
-            <div class="image">
-<img src="uploads/<?php echo $data['task_image'] ?>" alt="Imagem tarefa">
-
-            </div>
-        </div>
-        <div class="footer">
-        <p>Desenvolvimento por AnaElisaMueller</p>
+        <div class="image">
+            <img src="uploads/<?php echo $data[0]['task_image'] ?>" alt="Imagem da tarefa">
         </div>
     </div>
 
+    <div class="footer">
+        <p>Desenvolvido por AnaElisaMueller</p>
+    </div>
+    
+    <a href="index.php">voltar</a>
+</div>
+    
 </body>
-</html> 
+</html>
