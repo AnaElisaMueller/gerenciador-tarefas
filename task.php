52c712507136c91598ec56f00ba6bba1c3c1d4b1
AnaM <emailanaelisa@gmail.com>
2024-02-10 14:44:44 -0300

    Update task.php

--- task.php
+++ task.php
@@ -2,94 +2,55 @@
 
 require __DIR__ . '/connect.php';
 
-
 session_start();
 
-if ( isset ($_POST['task_name'])){
-    if ( $_POST['task_name'] != ""){
+if( isset($_POST['task_name'])) {
+    if ( $_POST['task_name'] != "") {
 
-        if (isset( $_FILES['task_image'])){
-            $ext =strtolower(substr($_FILES['task_image']['name'], -4));
-    
+        if ( Isset( $_FILES['task_image'] ) ) {
+            $ext =strtolower( substr( $_FILES['task_image']['name'], -4 ));
             $file_name = md5( date('Y.m.d.H.i.s')) . $ext;
-    
-            $dir = 'uploads/';
-    
-            move_uploaded_file ( $_FILES['task_image']['tmp_name'], $dir.$file_name );
-    
+            $dir  = 'uploads/';
+            
+            move_uploaded_file( $_FILES['task_image']['tmp_name'], $dir.$file_name );
         }
 
+        
+        
+        $stmt = $conn->prepare('INSERT INTO tasks (task_name, task_description, task_image, task_date)
+                            VALUES (:name, :description, :image, :date)');
+        $stmt->bindParam('name', $_POST['task_name']);
+        $stmt->bindParam('description', $_POST['task_description']);
+        $stmt->bindParam('image', $file_name);
+        $stmt->bindParam('date', $_POST['task_date']);
 
-
-            $stmt = $conn->prepare(' INSERT INTO taks ( task_name, task_description, task_image, task_date) 
-            VALUES (:name, :description. :image, :date)');
-            $stmt->bindParam('name', $_POST['task_name']);
-            $stmt->bindParam('description', $_POST['task_description']);
-            $stmt->bindParam('image', $file_name);
-            $stmt->bindParam('date', $_POST['task_date']);
-
-if ($stmt->execute()){
- $_SESSION['sucess'] = "Dados cadastrados.";
-  header('location:index.php');
-
-} else{
-    $_SESSION['error'] = "Dados não cadastrados.";
-    header('location:index.php');
-}
-            
-
-    }else{
-    $_SESSION['message'] = "O campo nome da tarefa não pode ser vazio!";
-    header('location:index.php');
+        if ($stmt->execute()) {
+            $_SESSION['success'] = "Dados Cadastrados.";
+            header('Location:index.php');
+        }
+        else {
+            $_SESSION['error'] = "Dados não Cadastrados.";
+            header('Location:index.php');
+        }
+        
     }
-
-}
-
-if ( isset ($_GET['key'])){
-    array_splice($_SESSION['tasks'], $_GET['key'], 1);
-    unset($_GET['key']);
-    header('location:index.php');
-}
-
-
-
-/*$_POST['task_name']
-
-if ( isset ($_POST['task_name'])){
-    if ($_POST['task_name'] != ""){
-
-    if (isset( $_FILES['task_image'])){
-        $ext =strtolower(substr($_FILES['task_image']['name'], -4));
-
-        $file_name = md5( date('Y.m.d.H.i.s')) . $ext;
-
-        $dir = 'uploads/';
-
-       // move_uploads_file ( $_FILES['task_image']['tmp_name'], $dir.$file_name );
-
+    else {
+        $_SESSION['message'] = "O campo nome da tarefa nao pode estar vazio.";
+        header('Location:index.php');
     }
+    
+}
 
-    $data = [
-        'task_name' => $_POST['task_name'],
-        'task_description' => $_POST['task_description'],
-        'task_date' => $_POST['task_date'],
-        'task_image' => $file_name
-        ];
+if ( isset($_GET['key'])) {
+    $stmt = $conn->prepare('DELETE FROM tasks WHERE id = :id');
+    $stmt->bindParam(':id', $_GET['key']);
 
-
-
-
-
-;
-    } else{
-    $_SESSION['message'] = "O campo nome da tarefa não pode ser vazio!";
-    header('location:index.php');
+    if ($stmt->execute()) {
+        $_SESSION['success'] = "Dados Removidos.";
+        header('Location:index.php');
+    }
+    else {
+        $_SESSION['error'] = "Dados não Removidos.";
+        header('Location:index.php');
     }
 }
-
-if ( isset ($_GET['key'])){
-    array_splice($_SESSION['tasks'], $_GET['key'], 1 );
-    unset($_GET['key']); 
-    header('location:index.php');
-}
-*/
